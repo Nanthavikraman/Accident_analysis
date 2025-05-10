d7b0f1c7a6902b8a28eb82a83bdcda9e82d2c089
Nanthavikraman M S <133190956+Nanthavikraman@users.noreply.github.com>
2025-05-10 23:00:44 +0530

    Create ROC_curve.py

---
+++ ROC_curve.py
@@ -0,0 +1,15 @@
+from sklearn.metrics import roc_curve, auc
+
+y_probs = model.predict_proba(X_test_scaled)[:, 1]
+fpr, tpr, thresholds = roc_curve(y_test, y_probs)
+roc_auc = auc(fpr, tpr)
+
+plt.figure()
+plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'ROC curve (AUC = {roc_auc:.2f})')
+plt.plot([0, 1], [0, 1], color='navy', linestyle='--')
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver Operating Characteristic')
+plt.legend()
+plt.grid()
+plt.show()
